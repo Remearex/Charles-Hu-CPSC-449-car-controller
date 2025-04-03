d0f55c699ddbea2e846d0d3fc6398dfd869ab205
Charles Hu <hucharles5@gmail.com>
2025-04-03 13:28:52 -0700

    added simulation (reward function)

--- car.py
+++ car.py
@@ -4,13 +4,18 @@ import numpy as np
 
 from tracks import training_track, WIDTH, HEIGHT
 
+track = training_track
+
 L, W = 60, 30
-x, y = WIDTH // 2, HEIGHT // 2
 d = 50
-theta = 0
 velocity = 200
-steering_angle = math.radians(30)
 dt = 1/60
+
+# initial state
+x, y = WIDTH // 2, HEIGHT // 2
+theta = 0
+steering_angle = 0
+
 # sensor information:
 ray_angles = [math.radians(60), math.radians(30), 0, math.radians(-30), math.radians(-60)]
 source1 = None
@@ -39,7 +44,7 @@ def update():
     update_rays()
 
 def update_rays():
-    global x, y, L, theta, source1, ray_angles, first_intersects, ray_lengths, training_track
+    global x, y, L, theta, source1, ray_angles, first_intersects, ray_lengths, track
 
     source1 = (x - (L//2)*math.sin(theta), y - (L//2)*math.cos(theta))
     for i in range(len(ray_angles)):
@@ -49,7 +54,7 @@ def update_rays():
         first_intersection_length = math.inf
         first_intersection_point = None
 
-        for line in training_track:
+        for line in track:
             v_2 = (line[2]-line[0], line[3]-line[1])
             try:
                 line_params = np.linalg.inv(np.array([[v_1[0], -v_2[0]], [v_1[1], -v_2[1]]])) @ (np.array([line[0], line[1]]) - np.array(source1))
@@ -68,50 +73,152 @@ def update_rays():
         first_intersects[i] = first_intersection_point
         ray_lengths[i] = first_intersection_length
 
-# Main loop
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
 
-pygame.init()
+def collision():
+    global x, y, L, W, theta, track
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("2D Car in Motion")
+    # this is a ccw rotation matrix by theta in a coordinate frame where i points to the right and j points up
+    # since the frame of the pygame canvas has j pointing down, we have to use -y for the offset. We're essentially
+    # going to do everything in the j pointing up frame
+    Rot = np.array([[math.cos(theta), -math.sin(theta), x],[math.sin(theta), math.cos(theta), -y]])
+    # we then convert everything back to the j pointing down frame by left-multiplying by [[1,0],[0,-1]] and flatten them
+    # into row vectors
+    top_left = (np.array([[1, 0],[0, -1]]) @ Rot @ np.array([[-W/2], [L/2], [1]])).flatten()
+    top_right = (np.array([[1, 0],[0, -1]]) @ Rot @ np.array([[W/2], [L/2], [1]])).flatten()
+    bot_left = (np.array([[1, 0],[0, -1]]) @ Rot @ np.array([[-W/2], [-L/2], [1]])).flatten()
+    bot_right = (np.array([[1, 0],[0, -1]]) @ Rot @ np.array([[W/2], [-L/2], [1]])).flatten()
 
-running = True
-clock = pygame.time.Clock()
+    for car_boundary in [(top_left, bot_left), (top_right, top_left), (bot_right, top_right), (bot_left, bot_right)]:
+        v_1unnorm = car_boundary[1] - car_boundary[0]
+        v_1norm = np.linalg.norm(v_1unnorm)
+        v_1 = v_1unnorm / v_1norm
+        for line in track:
+            v_2 = (line[2]-line[0], line[3]-line[1])
+            try:
+                line_params = np.linalg.inv(np.array([[v_1[0], -v_2[0]], [v_1[1], -v_2[1]]])) @ (np.array([line[0], line[1]]) - car_boundary[0])
+            except np.linalg.LinAlgError:
+                # if the matrix is invertible then the lines don't intersect
+                continue
+            if 0 <= line_params[0] and line_params[0] <= v_1norm and 0 <= line_params[1] and line_params[1] <= 1:
+                return line
+            
+    return False
 
-while running:
-    clock.tick(60)
-    screen.fill(WHITE)
+# this is the reward function. Runs a single simulation and returns the number of ticks the car drives without crashing
+# where the car starts from a fixed initial state
+def simulate(model_params, display=True):
 
-    update()
-
-    car_surface = pygame.Surface((W, L), pygame.SRCALPHA)
-    car_surface.fill(BLACK)
-    front_width = 10
-    front_rect = pygame.Rect(0, 0, W, front_width)
-    pygame.draw.rect(car_surface, RED, front_rect)
-
-    rotated_car = pygame.transform.rotate(car_surface, math.degrees(theta))
-    rotated_rect = rotated_car.get_rect(center=(x, y))
-
-    pygame.draw.circle(screen, BLACK, source1, 3)
-
-    screen.blit(rotated_car, rotated_rect.topleft)
-
-    # draw sensor rays
-    for first_intersect in first_intersects:
-        pygame.draw.line(screen, RED, source1, first_intersect, 3)
+    global x, y, theta, WIDTH, HEIGHT
     
-    # draw tracks
-    for wall in training_track:
-        pygame.draw.line(screen, BLACK, (wall[0], wall[1]), (wall[2], wall[3]), 1)
+    if display:
+        WHITE = (255, 255, 255)
+        RED = (255, 0, 0)
+        BLACK = (0, 0, 0)
 
-    pygame.display.flip()
+        pygame.init()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("2D Car in Motion")
+
+        running = True
+        clock = pygame.time.Clock()
+
+    x = WIDTH / 2
+    y = 4*HEIGHT/5
+    num_ticks = 0
+    running = True
+    while running:
+        num_ticks += 1
+        update()
+    
+        if display:
+            clock.tick(60)
+            screen.fill(WHITE)
+
+            car_surface = pygame.Surface((W, L), pygame.SRCALPHA)
+            car_surface.fill(BLACK)
+            front_width = 10
+            front_rect = pygame.Rect(0, 0, W, front_width)
+            pygame.draw.rect(car_surface, RED, front_rect)
+
+            rotated_car = pygame.transform.rotate(car_surface, math.degrees(theta))
+            rotated_rect = rotated_car.get_rect(center=(x, y))
+
+            pygame.draw.circle(screen, BLACK, source1, 3)
+
+            screen.blit(rotated_car, rotated_rect.topleft)
+
+            # draw sensor rays
+            for first_intersect in first_intersects:
+                pygame.draw.line(screen, RED, source1, first_intersect, 3)
+            
+            # draw tracks
+            for wall in track:
+                pygame.draw.line(screen, BLACK, (wall[0], wall[1]), (wall[2], wall[3]), 1)
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        
+        if collision():
             running = False
+    
+    return num_ticks
 
-pygame.quit()
+print(simulate(None, True))
+    
+
+
+
+# Main loop
+# WHITE = (255, 255, 255)
+# RED = (255, 0, 0)
+# BLACK = (0, 0, 0)
+# BLUE = (0, 0, 255)
+
+# pygame.init()
+
+# screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# pygame.display.set_caption("2D Car in Motion")
+
+# running = True
+# clock = pygame.time.Clock()
+
+# while running:
+#     clock.tick(60)
+#     screen.fill(WHITE)
+
+#     update()
+#     if collision():
+#         break
+
+#     car_surface = pygame.Surface((W, L), pygame.SRCALPHA)
+#     car_surface.fill(BLACK)
+#     front_width = 10
+#     front_rect = pygame.Rect(0, 0, W, front_width)
+#     pygame.draw.rect(car_surface, RED, front_rect)
+
+#     rotated_car = pygame.transform.rotate(car_surface, math.degrees(theta))
+#     rotated_rect = rotated_car.get_rect(center=(x, y))
+
+#     pygame.draw.circle(screen, BLACK, source1, 3)
+
+#     screen.blit(rotated_car, rotated_rect.topleft)
+
+#     # draw sensor rays
+#     for first_intersect in first_intersects:
+#         pygame.draw.line(screen, RED, source1, first_intersect, 3)
+    
+#     # draw tracks
+#     for wall in track:
+#         pygame.draw.line(screen, BLACK, (wall[0], wall[1]), (wall[2], wall[3]), 1)
+
+#     pygame.display.flip()
+
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+# pygame.quit()
